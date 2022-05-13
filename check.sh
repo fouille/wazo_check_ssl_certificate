f0b60520cc4a36182e91846ca2844e634c28a525
Fouille <franckisys@gmail.com>
2022-05-13 10:36:38 +0200

    add values

--- check.sh
+++ check.sh
@@ -2,9 +2,9 @@
 
 check_expire_date() {
 
-certificate_end_date=$(openssl s_client -connect localhost:443 2> /dev/null | openssl x509 -noout -enddate | cut -d '=' -f2)
+certificate_end_date=$(echo | openssl s_client -connect localhost:443 2> /dev/null | openssl x509 -noout -enddate | cut -d '=' -f2)
 
-whiptail --title "Certificate Information" --msgbox "Your certificate expire at : "$certificate_end_date". If is expired, you must regenerate it." 8 78
+whiptail --title "Certificate Information" --msgbox "Your self-signed certificate expire at : $certificate_end_date. If is expired, you must regenerate it." 8 78
 
 generate_new
 
@@ -12,14 +12,59 @@ generate_new
 
 generate_new(){
 
-if (whiptail --title "Generate new certificate" --yesno "Would you generate a new certificate ?" 8 78); then
+if (whiptail --title "Generate new self-signed certificate" --yesno "Would you generate a new self-signed certificate ?" 8 78); then
     #prevenir d'un redemarrage des services.
     echo "User selected Yes, exit : $?."
+    restart_services
 else
     echo "User selected No, exit : $?."
+    echo "Have a nice day, Wazo team."
 fi
 
 }
 
+restart_services(){
+if (whiptail --title "Generate new self-signed certificate" --yesno "All services will be restarted, please agree." 8 78); then
+    echo "User selected Yes restart services, exit : $?."
+    run_renew_certificate $?
+else
+    echo "User selected No restart services, exit : $?."
+    echo "Have a nice day, Wazo team."
+fi
+}
 
+run_renew_certificate(){
+if [ $1 = 0 ]; then
+       {
+            echo -e "XXX\n50\nAll Wazo Services stopping.\nXXX"
+            wazo-service stop all
+            echo -e "XXX\n100\nAll Wazo Services stopping... Done.\nXXX"
+            sleep 0.5
+
+            echo -e "XXX\n50\nBackup and remove certificates... \nXXX"
+            cp /usr/share/xivo-certs/server.{key,crt} /var/backups
+            rm /usr/share/xivo-certs/server.{key,crt}
+            echo -e "XXX\n100\nBackup and remove certificates... Done.\nXXX"
+            sleep 0.5
+
+            echo -e "XXX\n50\nRegenerate self-signed certificate... \nXXX"
+            dpkg-reconfigure xivo-certs
+            echo -e "XXX\n100\nRegenerate self-signed certificate... Done.\nXXX"
+            sleep 0.5
+
+            echo -e "XXX\n50\nRestart Wazo Services... \nXXX"
+            xivo-update-config
+            wazo-service stop all
+            echo -e "XXX\n50\nRestart Wazo Services... Done.\nXXX"
+            sleep 0.5
+
+        } | whiptail --gauge "Wait Please" 6 60 0
+
+        bye
+fi
+}
+
+bye(){
+whiptail --title "Congratulation" --msgbox "You have generate new self-signed certificate, congratulation. Have a nice day! Wazo Team." 8 78}
+}
 check_expire_date

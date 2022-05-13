33a45065c04ac6471f91472181d05d69a1254c0e
Fouille <franckisys@gmail.com>
2022-05-13 10:56:42 +0200

    add new date correction

--- check.sh
+++ check.sh
@@ -36,24 +36,24 @@ fi
 run_renew_certificate(){
 if [ $1 = 0 ]; then
        {
-            echo -e "XXX\n50\nAll Wazo Services stopping.\nXXX"
+            echo -e "XXX\n50\nAll Wazo Services stopping. Wait...\nXXX"
             wazo-service stop all
             echo -e "XXX\n100\nAll Wazo Services stopping... Done.\nXXX"
             sleep 0.7
 
-            echo -e "XXX\n50\nBackup and remove certificates... \nXXX"
+            echo -e "XXX\n50\nBackup and remove certificates... Wait...\nXXX"
             cp /usr/share/xivo-certs/server.{key,crt} /var/backups
             rm /usr/share/xivo-certs/server.{key,crt}
             echo -e "XXX\n100\nBackup and remove certificates... Done.\nXXX"
             sleep 0.7
 
-            echo -e "XXX\n50\nRegenerate self-signed certificate... \nXXX"
-            dpkg-reconfigure xivo-certs
+            echo -e "XXX\n50\nRegenerate self-signed certificate... Wait...\nXXX"
+            dpkg-reconfigure xivo-certs /dev/null 2>&1
             echo -e "XXX\n100\nRegenerate self-signed certificate... Done.\nXXX"
             sleep 0.7
 
-            echo -e "XXX\n50\nRestart Wazo Services... \nXXX"
-            xivo-update-config
+            echo -e "XXX\n50\nRestart Wazo Services... Wait...\nXXX"
+            xivo-update-config /dev/null 2>&1
             wazo-service start all
             echo -e "XXX\n50\nRestart Wazo Services... Done.\nXXX"
             sleep 0.7
@@ -68,6 +68,7 @@ bye(){
 
 certificate_end_date_new=$(echo | openssl s_client -connect localhost:443 2> /dev/null | openssl x509 -noout -enddate | cut -d '=' -f2)
 
-whiptail --title "Congratulation" --msgbox "You have generate new self-signed certificate (New date: $certificate_end_date_new), congratulation. Have a nice day! Wazo Team." 8 78}
+whiptail --title "Congratulation" --msgbox "You have generate new self-signed certificate (New date: $certificate_end_date_new), congratulation. Have a nice day! Wazo Team." 8 78
+
 }
 check_expire_date
